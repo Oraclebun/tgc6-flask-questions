fd61568995e7966469e67134a9b45de1426bb48c
Oracle <47670366+Oraclebun@users.noreply.github.com>
2020-05-23 11:51:05 +0800

    answered Q4, Q5, Q6

--- 06-post-requests/app.py
+++ 06-post-requests/app.py
@@ -10,11 +10,31 @@ def hello():
 	
 @app.route("/",methods=['POST'])
 def processHello():
-	return render_template('process-hello.template.html')
+    print(request.form)
+    fn=request.form.get('first-name')
+    ln=request.form.get('last-name')
+    return render_template('process-hello.template.html', fn=fn, ln=ln)
 
+@app.route("/calculate")
+def getNumber():
+    return render_template("process-calc.template.html")
+
+@app.route("/calculate",methods=['POST'])
+def processCalculate():
+    print(request.form)
+    number1 = request.form.get('number1')
+    number2 = request.form.get('number2')
+    result = str(int(number1)+int(number2))
+    return render_template('display-calc.template.html', result=result)
 
 # "magic code" -- boilerplate
+#if __name__ == '__main__':
+#    app.run(host=os.environ.get('IP'),
+#            port=int(os.environ.get('PORT')),
+#            debug=True)
+
+
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('127.0.0.1'),
+            port=int(os.environ.get('PORT','8000')),
             debug=True)
